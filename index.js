47fc6bd0b986ce21bcd63fb6afec9b235cc971c0
tanvir <tanvirulislam149@gmail.com>
2023-10-20 18:43:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.ei9jh8k.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connected");
+
+    const dataCollection = client.db("chart").collection("data");
+
+    app.get("/data", async (req, res) => {
+      const cursor = dataCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World from blackCoffer')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
